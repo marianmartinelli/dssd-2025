72a49c1ccade6107ad123254b086187a1be2fc9b
federicoperroni <perroni_fede@hotmail.com>
2025-10-14 00:23:31 -0300

    se saca comentarios innecesarios

--- backend/app/services/project_service.py
+++ backend/app/services/project_service.py
@@ -29,23 +29,23 @@ async def save_project(payload: ProjectCreate, bonita_response: Dict, current_us
             contact_phone=payload.contact_phone,
             estimated_budget=payload.estimated_budget,
             currency=payload.currency,
-            start_date=payload.start_date,  # Usar directamente el valor sin conversión
-            end_date=payload.end_date,  # Usar directamente el valor sin conversión
+            start_date=payload.start_date,
+            end_date=payload.end_date,
             priority_level=payload.priority_level,
             supporting_docs_url=payload.supporting_docs_url,
             submission_timestamp=datetime.strptime(payload.start_date.strftime("%Y-%m-%dT%H:%M:%S"), "%Y-%m-%dT%H:%M:%S"),
             initiator_user_id=current_user["username"],
         )
         session.add(project)
-        await session.flush()  # Ensure the project ID is generated
+        await session.flush() 
 
         # Save work plan stages
         for stage in payload.work_plan_stages:
             work_plan_stage = WorkPlanStage(
                 project_id=project.id,
                 stage_name=stage.stage_name,
-                stage_start=stage.stage_start,  # Usar directamente el valor sin conversión
-                stage_end=stage.stage_end,  # Usar directamente el valor sin conversión
+                stage_start=stage.stage_start,
+                stage_end=stage.stage_end, 
                 support_type=stage.support_type,
                 description=stage.description,
                 estimated_amount=stage.estimated_amount,
